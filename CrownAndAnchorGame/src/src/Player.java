edad63f660786c4a05534f0f20fafb8c9dfad775
gpathela <gpathela@yahoo.com>
2016-10-06 14:54:28 +1100

    Bug rule 10 - Solved

--- CrownAndAnchorGame/src/src/Player.java
+++ CrownAndAnchorGame/src/src/Player.java
@@ -4,9 +4,10 @@ public class Player {
 	private int balance;
 	private int limit;
 	
-	public Player(String name, int balance) {
+	public Player(String name, int balance, int age) {
 		if (name == null || name .isEmpty()) throw new IllegalArgumentException("Name cannot be null or empty");
 		if (balance < 0) throw new IllegalArgumentException("Balance cannot be negative");
+		if (age < 18) throw new IllegalArgumentException("Age Can not be less than 18");
 		this.name = name;
 		this.balance = balance;
 		this.limit = 0;
