3d71716ff39f767faf661ba2ac17fc44ffee4a84
rpreet <rpreet72@gmail.com>
2016-10-06 22:32:57 +1100

    COMMITMENTS IN Game CLASS

--- CrownAndAnchorGame/src/src/Game.java
+++ CrownAndAnchorGame/src/src/Game.java
@@ -3,11 +3,27 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Game {
+/**
+* @reviewer Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 07.10.2016
+* @version 1.1
+*
+*/
 
+/** Start of Game class */
+public class Game {
+	
+	/** 
+	* Declare and initialize the variable for the class
+	* with private visibility 
+	*/
 	private List<Dice> dice;
 	private List<DiceValue> values;
 	
+	/** Construct an Game object with the specified attribute. */
 	public Game(Dice die1, Dice die2, Dice die3) {
 		if (die1 == null || die2 == null || die3 == null) throw new IllegalArgumentException("Dice cannot be null.");
 		dice = new ArrayList<Dice>();
@@ -48,4 +64,4 @@ public class Game {
         return winnings;		
 	}
 	
-}
+}//End of Game class
