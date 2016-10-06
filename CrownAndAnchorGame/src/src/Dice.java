ab4fd72df39d21c86f789766641bb012196637b9
rpreet <rpreet72@gmail.com>
2016-10-06 22:19:37 +1100

    commitments

--- CrownAndAnchorGame/src/src/Dice.java
+++ CrownAndAnchorGame/src/src/Dice.java
@@ -1,3 +1,12 @@
+/**
+* @reviewer Ramanpreet Kaur
+* @course Master of Information Technology
+* @subject Professional Programming Practice
+* @instructor Dr Recep Ulusoy
+* @due date 07.10.2016
+* @version 1.1
+*
+*/
 public class Dice {
 			
 	private DiceValue value;
