98a86beed6603d1ae9e1a4b126e1628e79c6a837
gpathela <gpathela@yahoo.com>
2016-10-07 20:52:06 +1100

    Bug Solved - Now bets can be placed on Spade

--- CrownAndAnchorGame/src/src/DiceValue.java
+++ CrownAndAnchorGame/src/src/DiceValue.java
@@ -22,7 +22,9 @@ public enum DiceValue {
 	}
 	
 	public static DiceValue getRandom() {
-		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
+		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal() + 1);
+		
+		//System.out.println(random);
 		return values()[random];
 	}
 	
