91c8d902f28c938ade71efd61b3b13821618e750
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-10-08 00:25:27 +1100

    PaymentSolved

--- CrownAndAnchorGame/src/src/Game.java
+++ CrownAndAnchorGame/src/src/Game.java
@@ -1,3 +1,4 @@
+package dice;
 /**
 *@reviewer Chiranjivi Bashyal
 *@course Master of Information Technology
@@ -38,7 +39,7 @@ public class Game {
 		if (pick == null) throw new IllegalArgumentException("Pick cannot be negative.");
 		if (bet < 0) throw new IllegalArgumentException("Bet cannot be negative.");
 		
-		player.takeBet(bet);
+		
 		    
 		int matches = 0;
 		for ( Dice d : dice) {
@@ -52,6 +53,8 @@ public class Game {
 
 		if (matches > 0) {			
 			player.receiveWinnings(winnings);
+		}else {
+			player.takeBet(bet);
 		}
         return winnings;		
 	}
