e91f214ee2f6f407cb2cf7a1696c7fb51e318bde
Chiranjivi Bashyal <chiranjivibashyal7@gmail.com>
2016-10-08 22:58:37 +1100

    comments on Dice

--- CrownAndAnchorGame/src/src/Dice.java
+++ CrownAndAnchorGame/src/src/Dice.java
@@ -6,24 +6,29 @@
 *@due date 07.10.2016
 *@version 1.1
 */
-
+package dice;
+/** Start of the class Dice */
 public class Dice {
-			
+	/** Declare a variables of type DiceValue that will be used in the code */			
 	private DiceValue value;
 	
+	/** A no-args constructor for a Dice object */ 
 	public Dice() {
 		value =  DiceValue.getRandom();
 	}
 	
+	/** The getValue method returns the value of the Dice object */
 	public DiceValue getValue() {
 		return value;
 	}
 
+	/** The roll returns a random value of the value of the Dice object */
 	public DiceValue roll() {
 		return DiceValue.getRandom();
 	}		
 	
+	/** The toString method returns a string of the value of the Dice object */
 	public String toString() {
 		return value.toString();
 	}
-}
+} // End of the Dice class 
