5cea193d6fe561369cb87bb88813aa24c6553d45
gpathela <gpathela@yahoo.com>
2016-10-10 23:49:27 +1100

    Bug 5 Resolution

--- CrownAndAnchorGame/src/src/Main.java
+++ CrownAndAnchorGame/src/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.io.*;
 
 public class Main {
@@ -66,11 +67,21 @@ public class Main {
 	                	loseCount++;
                     }
                     
+                    System.out.println("Make choice : q to Quit or any other key to continue");
+                    //Scanner scanner = new Scanner(System.in);
+                    char c = (char) System.in.read();
+                    System.out.println(c);
+                    if (c == 'q'){
+                    	System.out.println("Thanks for playing with us. Bye");
+                    	i = 100;
+                    	break;
+                    }
+                    
                 } //while
 
                 System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
                 System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
-                
+                System.out.println(i);
             } //for
             
             System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
