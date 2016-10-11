3a6d3a7f72ca5744f1c9033ad703e7163f3579ad
JinoRousseau1774 <jinorousseau@hotmail.com>
2016-10-12 07:49:35 +1100

    Talking about Bug 4 in the report

--- CrownAndAnchorGame/src/src/DiceValue.java
+++ CrownAndAnchorGame/src/src/DiceValue.java
@@ -22,6 +22,7 @@ public enum DiceValue {
 	private static Random RANDOM = new Random();
 	
 	private static final Map<DiceValue, String> VALUE_REPR_MAP= new HashMap<DiceValue, String>();
+    
 	static {
 		VALUE_REPR_MAP.put(DiceValue.CROWN, "Crown");
 		VALUE_REPR_MAP.put(DiceValue.ANCHOR, "Anchor");
