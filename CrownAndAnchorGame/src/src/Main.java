653876552889d3534c16e47514f8d77b771db064
gpathela <gpathela@yahoo.com>
2016-10-22 11:14:51 +1100

    Updatded Main
    Final Version

--- CrownAndAnchorGame/src/src/Main.java
+++ CrownAndAnchorGame/src/src/Main.java
@@ -1,5 +1,19 @@
+/** Import some Java libraries and specify the package in which the classes are organized */
 import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 import java.io.*;
+
+/**
+*@reviewer Gourav Pathela
+*@course Master of Information Technology
+*@subject Programming Principle Practice
+*@lecturer Dr Recep Ulusoy
+*@due date 07.10.2016
+*@version 1.1
+*
+*The class Main is the program that contains the code to run the game and make note of win lose.  
+*/
 
 public class Main {
 	
@@ -11,7 +25,7 @@ public class Main {
         Dice d2 = new Dice();
         Dice d3 = new Dice();
 
-        Player player = new Player("Fred", 100);
+        Player player = new Player("Fred", 100, 18);
         Game game = new Game(d1, d2, d3);
         List<DiceValue> cdv = game.getDiceValues();
 
@@ -28,7 +42,8 @@ public class Main {
             	String name = "Fred";
             	int balance = 100;
             	int limit = 0;
-                player = new Player(name, balance);
+            	int age = 18;
+                player = new Player(name, balance, age);
                 player.setLimit(limit);
                 int bet = 5;
 
@@ -48,9 +63,11 @@ public class Main {
                 	int winnings = game.playRound(player, pick, bet);
                     cdv = game.getDiceValues();
                     
-                    System.out.printf("Rolled %s, %s, %s\n",
-                    		cdv.get(0), cdv.get(1), cdv.get(2));
                     
+                    System.out.printf("Rolled %s, %s, %s\n",
+                    		cdv.get(0),cdv.get(1),cdv.get(2));
+                    
+                    		
                     if (winnings > 0) {
 	                    System.out.printf("%s won %d, balance now %d\n\n",
 	                    		player.getName(), winnings, player.getBalance());
@@ -62,11 +79,21 @@ public class Main {
 	                	loseCount++;
                     }
                     
+                    System.out.println("Make choice : q to Quit or any other key to continue");
+                    //Scanner scanner = new Scanner(System.in);
+                    char c = (char) System.in.read();
+                    System.out.println(c);
+                    if (c == 'q'){
+                    	System.out.println("Thanks for playing with us. Bye");
+                    	i = 100;
+                    	break;
+                    }
+                    
                 } //while
 
                 System.out.print(String.format("%d turns later.\nEnd Game %d: ", turn, i));
                 System.out.println(String.format("%s now has balance %d\n", player.getName(), player.getBalance()));
-                
+                System.out.println(i);
             } //for
             
             System.out.println(String.format("Win count = %d, Lose Count = %d, %.2f", winCount, loseCount, (float) winCount/(winCount+loseCount)));
